e25f38a3d025b04413907789b974f2ddaf99cf24
MergeCells <arashimata@gmail.com>
2015-06-08 19:51:40 +0900

    あうぇｆかうぇｓ

--- GithubTest/Source.cpp
+++ GithubTest/Source.cpp
@@ -12,8 +12,16 @@ Githubの使い方
 http://codezine.jp/article/detail/8436?p=3
 ↑参考サイト
 
-VisualBasic2013でチームエクスプローラを表示する．
+VisualBasic2013で「チームエクスプローラ」を表示する．
 （↑ソリューションエクスプローラとかのとこ）
+
+「チームエクスプローラ」の中の「接続」のタブを開く
+
+「ローカルGitリポジトリ」の「複製▼」で黄色いところに↓
+https://github.com/HORIOJAPAN/GithubTeamTest.git
+をコピーする
+
+「複製」する
 
 
 
